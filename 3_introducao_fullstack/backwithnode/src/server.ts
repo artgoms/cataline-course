18b103074b0a4f5144f31ee8c6dd54cef082a8c5
Artur Gomes <arturg91@outlook.com>
2022-07-02 19:49:43 -0300

    backend intro application

---
+++ 3_introducao_fullstack/backwithnode/src/server.ts
@@ -0,0 +1,68 @@
+import express from "express";
+import { v4 as uuid } from "uuid";
+import cors from "cors";
+
+const app = express();
+
+app.use(express.json());
+app.use(cors({origin: '*'}))
+
+// METODOS HTTP -> GET | POST | PUT | DELETE
+
+// https://localhost:3333/usersS
+
+interface User {
+  id: string;
+  name: string;
+  email: string;
+}
+
+const users: User[] = [];
+
+app.get("/users", (request, response) => {
+  return response.json(users);
+});
+
+app.post("/users", (request, response) => {
+  const { name, email } = request.body;
+
+  const user = { id: uuid(), name, email };
+
+  users.push(user);
+
+  return response.json(user);
+});
+
+app.put("/users/:id", (request, response) => {
+  const { id } = request.params;
+  const { name, email } = request.body;
+
+  const userIndex = users.findIndex((user) => user.id === id); // -1
+
+  if (userIndex < 0) {
+    return response.status(404).json({ error: "User not found." });
+  }
+
+  const user = { id, name, email };
+  users[userIndex] = user;
+
+  return response.json(user);
+});
+
+app.delete("/users/:id", (request, response) => {
+  const { id } = request.params;
+
+  const userIndex = users.findIndex((user) => user.id === id); // -1
+
+  if (userIndex < 0) {
+    return response.status(404).json({ error: "User not found." });
+  }
+
+  users.splice(userIndex, 1);
+
+  return response.status(204).send();
+});
+
+app.listen("3333", () => {
+  console.log("Back-end Started");
+});
