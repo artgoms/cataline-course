5b07c49598dddbc9701ab45102908d12ae758c36
Artur Gomes <arturg91@outlook.com>
2022-07-21 00:18:59 -0300

    ibook config

---
+++ 8_vue_com_nuxt/3_iniciando_ibook/5_pagina_sobre/pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="box">
+    <h1>Home Page</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  layout: 'ibook',
+})
+</script>
+
+
